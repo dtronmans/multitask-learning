ffa208488d79fac552481c7f1a62a2fb76c6ea33
Dani Rogmans <danirogmans@gmail.com>
2025-04-09 13:06:18 +0200

    initial files

---
+++ dataset.py
@@ -0,0 +1,112 @@
+import os
+import random
+
+import pandas as pd
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+
+class MedicalImageDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.images_dir = os.path.join(root_dir, 'images')
+        self.masks_dir = os.path.join(root_dir, 'masks')
+        self.patient_list = pd.read_csv(os.path.join(root_dir, 'lumc_rdgg_attributes_filtered.csv'))
+        self.transform = transform
+
+        self.samples = []
+
+        # Create a mapping from Study ID to clinical information
+        clinical_info = {}
+        for _, row in self.patient_list.iterrows():
+            study_id = row['Study ID']
+            clinical_info[study_id] = {
+                'menopausal_status': row.get('Menopausal status', 'Unknown'),
+                'malignancy': 'malignant' if row['Malignancy status'] == 1 else 'benign',
+                'hospital': 'LUMC' if study_id.startswith('LUMC') else 'RDG'
+            }
+
+        for label in ['benign', 'malignant']:
+            image_folder = os.path.join(self.images_dir, label)
+            mask_folder = os.path.join(self.masks_dir, label)
+
+            for filename in os.listdir(image_folder):
+                if filename.lower().endswith(('png', 'jpg', 'jpeg', '.tif')):
+                    image_path = os.path.join(image_folder, filename)
+                    mask_path = os.path.join(mask_folder, filename)
+                    has_mask = os.path.exists(mask_path)
+
+                    base_id = filename.split('_')[0]  # e.g., LUMC12345
+                    info = clinical_info.get(base_id, {'menopausal_status': 'Unknown', 'hospital': 'Unknown'})
+
+                    self.samples.append({
+                        'image_path': image_path,
+                        'mask_path': mask_path if has_mask else None,
+                        'label': label,
+                        'filename': filename,
+                        'menopausal_status': info['menopausal_status'],
+                        'hospital': info['hospital']
+                    })
+
+        random.shuffle(self.samples)  # Shuffle the dataset
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        sample = self.samples[idx]
+        image = Image.open(sample['image_path']).convert('L')
+        mask = None
+
+        if sample['mask_path']:
+            mask = Image.open(sample['mask_path']).convert('L')
+
+        if self.transform:
+            image = self.transform(image)
+            if mask:
+                mask = self.transform(mask)
+
+        return {
+            'image': image,
+            'label': 1 if sample['label'] == "benign" else 0
+        }
+
+    def display(self, idx):
+        sample = self.__getitem__(idx)
+        image = transforms.ToPILImage()(sample['image']) if isinstance(sample['image'], torch.Tensor) else sample[
+            'image']
+        mask = transforms.ToPILImage()(sample['mask']) if sample['mask'] is not None and isinstance(sample['mask'],
+                                                                                                    torch.Tensor) else \
+            sample['mask']
+
+        fig, ax = plt.subplots(1, 2 if mask else 1, figsize=(12, 6))
+
+        title_text = f"{sample['filename']} ({sample['label']})\nHospital: {sample['hospital']}, Menopausal Status: {sample['menopausal_status']}"
+
+        # Show original image
+        if mask:
+            ax[0].imshow(image)
+            ax[0].set_title(title_text)
+            ax[0].axis('off')
+
+            # Show image with mask overlay
+            ax[1].imshow(image)
+            ax[1].imshow(mask, cmap='jet', alpha=0.5)
+            ax[1].set_title("With Mask Overlay")
+            ax[1].axis('off')
+        else:
+            ax.imshow(image)
+            ax.set_title(title_text)
+            ax.axis('off')
+
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    dataset = MedicalImageDataset("../final_datasets/lumc_rdg_only_ovary")
+    for i in range(len(dataset)):
+        dataset.display(i)
---
+++ train_scripts/classification_only.py
@@ -0,0 +1,92 @@
+import torch
+from sklearn.metrics import accuracy_score, recall_score
+from sklearn.model_selection import train_test_split
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision import models
+from torchvision import transforms
+from tqdm import tqdm
+
+from dataset import MedicalImageDataset
+
+if __name__ == "__main__":
+    num_epochs = 100
+    batch_size = 4
+    learning_rate = 0.001
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = models.resnet18()
+    model.fc = nn.Sequential(
+        nn.Linear(model.fc.in_features, 8)
+    )
+    model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+    model.load_state_dict(torch.load("models/mmotu/resnet18.pt", map_location=device))
+    model.fc = nn.Sequential(
+        nn.Linear(512, 2)
+    )
+
+    transform = transforms.Compose([
+        transforms.Resize((336, 544)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[.5], std=[.5])
+    ])
+
+    dataset = MedicalImageDataset("../final_datasets/lumc_rdg_only_ovary", transform=transform)
+
+    train_indices, val_indices = train_test_split(range(len(dataset)), test_size=0.2008, random_state=42)
+    train_dataset = torch.utils.data.Subset(dataset, train_indices)
+    val_dataset = torch.utils.data.Subset(dataset, val_indices)
+
+    print("Train dataset length: " + str(len(train_dataset)))
+    print("Val dataset length: " + str(len(val_dataset)))
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(num_epochs):
+        model.train()
+        train_loss = 0
+        train_preds = []
+        train_labels = []
+        for batch in tqdm(train_loader):
+            inputs, labels = batch['image'].to(device), batch['label'].to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+            train_preds.extend(predicted.cpu().numpy())
+            train_labels.extend(labels.cpu().numpy())
+
+        train_acc = accuracy_score(train_labels, train_preds)
+        train_recall = recall_score(train_labels, train_preds, average='macro')
+
+        model.eval()
+        val_loss = 0
+        val_preds = []
+        val_labels = []
+        with torch.no_grad():
+            for batch in tqdm(val_loader):
+                inputs, labels = batch['image'].to(device), batch['label'].to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+                _, predicted = torch.max(outputs, 1)
+                val_preds.extend(predicted.cpu().numpy())
+                val_labels.extend(labels.cpu().numpy())
+
+        val_acc = accuracy_score(val_labels, val_preds)
+        val_recall = recall_score(val_labels, val_preds, average='macro')
+
+        print(f"Epoch [{epoch+1}/{num_epochs}]"
+              f" Train Loss: {train_loss/len(train_loader):.4f}"
+              f" Acc: {train_acc:.4f}"
+              f" Recall: {train_recall:.4f}"
+              f" | Val Loss: {val_loss/len(val_loader):.4f}"
+              f" Acc: {val_acc:.4f}"
+              f" Recall: {val_recall:.4f}")
