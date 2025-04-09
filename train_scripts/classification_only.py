f89bcb1b41d3fb71638a1db0cde486f98d2110af
Dani Rogmans <danirogmans@gmail.com>
2025-04-09 14:15:21 +0200

    cl;ass balance items

--- train_scripts/classification_only.py
+++ train_scripts/classification_only.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import torch
 from sklearn.metrics import accuracy_score, recall_score
 from sklearn.model_selection import train_test_split
@@ -36,7 +38,12 @@ if __name__ == "__main__":
 
     dataset = MedicalImageDataset("/exports/lkeb-hpc/dzrogmans/lumc_rdg_final", transform=transform)
 
-    train_indices, val_indices = train_test_split(range(len(dataset)), test_size=0.2008, random_state=42)
+    train_indices, val_indices = train_test_split(
+        range(len(dataset)),
+        test_size=0.2,
+        stratify=[dataset[idx]['label'] for idx in range(len(dataset))],
+        random_state=42
+    )
     train_dataset = torch.utils.data.Subset(dataset, train_indices)
     val_dataset = torch.utils.data.Subset(dataset, val_indices)
 
@@ -46,7 +53,14 @@ if __name__ == "__main__":
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-    criterion = nn.CrossEntropyLoss()
+    train_targets = [dataset[idx]['label'] for idx in train_indices]
+    label_counts = Counter(train_targets)
+
+    total = sum(label_counts.values())
+    class_weights = [total / label_counts[i] for i in range(len(label_counts))]
+    weights_tensor = torch.FloatTensor(class_weights).to(device)
+
+    criterion = nn.CrossEntropyLoss(weight=weights_tensor)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
     for epoch in range(num_epochs):
