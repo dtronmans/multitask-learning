18d13b31809690572ed06e49a9df92685110278f
Dani Rogmans <danirogmans@gmail.com>
2025-05-20 09:33:49 +0200

    model to device

--- architectures/model_builder.py
+++ architectures/model_builder.py
@@ -69,14 +69,13 @@ def return_model(task, backbone, denoised=False,
             efficientnet_model.to(device)
             if clinical:
                 model = EfficientNetClinical(efficientnet_model, num_classes=2)
-                model.to(device)
-                model = transfer_weights_to_clinical_model(efficientnet_model, model)
                 model.classification_head = nn.Sequential(
                     nn.Linear(1280 + 128, 128),
                     nn.ReLU(),
                     nn.Dropout(0.4),
                     nn.Linear(128, 2)
                 )
+                model.to(device)
                 return model
             else:
                 efficientnet_model.classification_head = nn.Sequential(
